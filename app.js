841f29a5abfa985b2b33b3ea8257447a149751fe
Dominic Tassio <dmattt98@gmail.com>
2014-02-14 14:09:42 -0600

    fix express.static syntax error

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var io = require('socket.io').listen(server);
 
 server.listen(80);
 
-app.use(express.statis(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
 	res.sendfile(__dirname + '/index.html');
