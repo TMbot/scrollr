1d14bb4e73b4145c3e79c956549743eb4c773e67
Dominic Tassio <dmattt98@gmail.com>
2014-02-14 13:43:36 -0600

    fix errors

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var asjdkfh = require('express')();
+var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
 	res.sendfile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function (sockt) {
+io.sockets.on('connection', function (socket) {
 	socket.emit('test', {"hello": "world"});
 });
