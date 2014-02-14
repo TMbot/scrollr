367bde04720ba606e7784233f319dfb04b04983f
Dominic Tassio <dmattt98@gmail.com>
2014-02-14 14:07:24 -0600

    fix express requirement issue

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var app = require('express')();
+var express = reqiure('express');
+var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
