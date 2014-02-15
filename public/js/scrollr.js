b2f4a354c44623f7a8e0a863223f1aa791fbcd22
Dominic Tassio <dmattt98@gmail.com>
2014-02-15 13:03:03 -0600

    major updates

--- public/js/scrollr.js
+++ public/js/scrollr.js
@@ -1,8 +1,22 @@
-(function (io) {
+/*(function (io) {
 
 	var socket = io.connect('http://142.4.215.62:80');
 	socket.on('test', function (data) {
 		console.log(data);
 	});
 
-})(io);
+})(io);*/
+
+$(function () {
+	$('.chat-expand').on('click', function (e) {
+		var $body = $('.chat-body');
+		var $expand = $('.chat-expand');
+		if ($body.css('display') === 'none') {
+			$body.slideDown();
+			$expand.html('&#9660;');
+		} else {
+			$body.slideUp();
+			$expand.html('&#9650;');
+		}
+	});
+});
