4fa98565806a5153d63b030e9d9bacec90cd97fd
dmattt98 <dmattt98@gmail.com>
2014-02-15 14:23:15 -0600

    fixes to javascript includes

--- public/js/scrollr.js
+++ public/js/scrollr.js
@@ -1,11 +1,11 @@
-/*(function (io) {
+(function (io) {
 
 	var socket = io.connect('http://142.4.215.62:80');
 	socket.on('test', function (data) {
 		console.log(data);
 	});
 
-})(io);*/
+})(io);
 
 $(function () {
 	$('.chat-expand').on('click', function (e) {
